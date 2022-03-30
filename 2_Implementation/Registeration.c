1b48d2c5aa60699e926fb0ebb61e8f5b280abc04
Shyam2526 <101013448+Shyam2526@users.noreply.github.com>
2022-03-30 19:00:24 +0530

    Update Registeration.c

--- 2_Implementation/Registeration.c
+++ 2_Implementation/Registeration.c
@@ -4,17 +4,16 @@
 #include<conio.h>
 #include "header.h"
 static int i=0;
+char checker; 
+static int z=0;
 int ch;
 char x;
 struct detail
 {
 char name,pass;
 }w;
-void reg()
-  {
+void reg(){
     FILE *fp;
-    char checker; 
-    static int z=0;
     if(fp == NULL){
       fp=fopen("Web_reg.txt", "w");
       goto a;
@@ -27,26 +26,21 @@ void reg()
     printf("\n\t\t\t\t^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
     printf("\n\n\t\t\t\tWELCOME TO THE REGISTERATION");
     printf("\n\t\t\t\t^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
-    for(i=0;i<100;i++)
-    {
+    for(i=0;i<100;i++){
       printf("\n\n\t\t\t\t  ENTER USERNAME: ");
       scanf("%30s",checker);
-      while(!feof(fp) )
-    	{ 
-        if(strcmp(checker,w.name) == 0)
-        {
+      while(!feof(fp) ){ 
+        if(strcmp(checker,w.name) == 0){
           printf("\n\n\t\t\tUSERNAME ALREDY EXISTS");
           reg();
         }
-        else
-        {
+        else{
           strcpy(w.name,checker);
           break;
         }
       }
       printf("\n\n\t\t\t\t  DESIRED PASSWORD: ");
-      while((c=getch())!=13)
-        {
+      while((c=getch())!=13){
           w.pass[z++]=c;
           printf("%c",'*');
         }
@@ -58,86 +52,50 @@ void reg()
       }
     getch();
   }
-void login()
-{
+
+char c, name, pass; 
+int e=0;
+int checku,checkp;
+
+void login(){
   FILE *fp;
-  char c,name,pass; int z=0;
-  int checku,checkp;
   fp=fopen("Web_reg.txt", "r+");
-  if(fp == NULL)
-  {
+  if(fp == NULL){
     printf("\t\t\tfile does not found !");
     exit(1);
   } 
-  else
-  {
-    printf("\n\n\t\t\t\tWELCOME TO LOG IN ZONE");
-    printf("\n\t\t\t\t^^^^^^^^^^^^^^^^^^^^^^");
+  else{
+    printf("\n\t\t\t\t^^^^^^^^^^^^^^^^^^^^^^\n\n\t\t\t\tWELCOME TO LOG IN ZONE\n\t\t\t\t^^^^^^^^^^^^^^^^^^^^^^");
     printf("\n\n\t\t\t\t  ENTER USERNAME: ");
     scanf("%30s",name);
-    while( (c = fgetc(fp)) != EOF)
-    {
+    while( (c = fgetc(fp)) != EOF){
       fscanf(fp,"%30s %30s",w.name,w.pass);
-      if(strcmp(w.name, name) == 0)
-      {
+      if(strcmp(w.name, name) == 0){
         checku=0;
         printf("\n\n\t\t\t\t  ENTER PASSWORD: ");
-        while((c=getch())!=13)
-        {
-          pass[z++]=c;
+        while((c=getch())!=13){
+          pass[e++]=c;
           printf("%c",'*');
         }
-          pass[z]='\0';
+          pass[e]='\0';
           printf("%s", w.pass);
           checkp=strcmp(w.pass,pass);
           break;
         }
       }
   }
-if(checku==0&&checkp==0)
-{
+if(checku==0&&checkp==0){
   printf("\n\n\n\t\t\t\tYOU HAVE LOGGED IN SUCCESSFULLY!!");
   printf("\n\n\n\t\t\t\tWELCOME, HAVE A NICE DAY");
   system("cls");
- do{
-	printf("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-	printf("\n");	
-	printf("\t\t\tBus Ticket booking ");
-	printf("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-	
-	printf("\n\t\t\tEnter >1< To Book Ticket");
-  printf("\n\t\t\tEnter >2< To View All Route");
-  printf("\n\t\t\tEnter >0< To Exit ");
-
-   	printf("\n\t\t\tEnter your Choice ::");
-   	scanf("%d",&ch); 
-	  switch(ch){
-    case 1 :
-    		ticket_booking();
-   		  break;
-		case 2:
-    		viewAll_route();
-   		  break;		
-    case 0:
-    		main();
-    		break;
-
-    default:
-    		printf("\n\t\tWrong choice !");
-    		break;
-   	}
-       
- }while(ch!=0);
 }
-else if(checku==0&&checkp!=0)
-{
+else if(checku==0&&checkp!=0){
   printf("\n\n\n\t\t\tWRONG PASSWORD!! Not %s??",name);
   printf("\n\n\t\t\t\t  (Press 'y' to re-enter password)");
   getch();
   login();
 }
-else if(checku!=0)
-{
+else if(checku!=0){
   printf("\n\n\n\t\t\tYou are not a Registered User\n \t\t\tPress enter to register yourself");
   if(getch()==13)
     reg();
